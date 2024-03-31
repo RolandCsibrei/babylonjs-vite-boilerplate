cc2438a1515de024820603c9c3523ee3702e67c1
Roland Csibrei <roland.csibrei@gmail.com>
2024-03-31 15:47:30 +0200

    Updated dependencies, es6 style code

--- src/AppOne.ts
+++ src/AppOne.ts
@@ -1,10 +1,13 @@
-import * as BABYLON from 'babylonjs'
+import { Scene, FreeCamera, Vector3, HemisphericLight, MeshBuilder, StandardMaterial, Color3, Texture, Engine, ArcRotateCamera } from "@babylonjs/core";
+import "@babylonjs/core/Debug/debugLayer";
+import "@babylonjs/inspector";
+
 export class AppOne {
-    engine: BABYLON.Engine;
-    scene: BABYLON.Scene;
+    engine: Engine;
+    scene: Scene;
 
     constructor(readonly canvas: HTMLCanvasElement) {
-        this.engine = new BABYLON.Engine(canvas)
+        this.engine = new Engine(canvas)
         window.addEventListener('resize', () => {
             this.engine.resize();
         });
@@ -26,69 +29,29 @@ export class AppOne {
             this.scene.render();
         });
     }
-
-
-
 }
 
-
-var createScene = function (engine: BABYLON.Engine, canvas: HTMLCanvasElement) {
-    // this is the default code from the playground:
-
-    // This creates a basic Babylon Scene object (non-mesh)
-    var scene = new BABYLON.Scene(engine);
-
-    // This creates and positions a free camera (non-mesh)
-    var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
-
-    // This targets the camera to scene origin
-    camera.setTarget(BABYLON.Vector3.Zero());
-
-    // This attaches the camera to the canvas
+const createScene = function (engine: Engine, canvas: HTMLCanvasElement) {
+    const scene = new Scene(engine);
+    const camera = new ArcRotateCamera("camera1", Math.PI / 3, Math.PI / 4, 30, new Vector3(0, 0, 0));
+    camera.setTarget(Vector3.Zero());
     camera.attachControl(canvas, true);
 
-    // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-    var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
-
-    // Default intensity is 1. Let's dim the light a small amount
+    const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
     light.intensity = 0.7;
+    const sphere = MeshBuilder.CreateBox("box", { size: 6 }, scene);
+    sphere.position.y = 4;
 
-    // Our built-in 'sphere' shape.
-    var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 2, segments: 32 }, scene);
-    // Move the sphere upward 1/2 its height
-    let startPos = 2;
-    sphere.position.y = startPos;
-
-    // Our built-in 'ground' shape.
-    var ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
-    var groundMaterial = new BABYLON.StandardMaterial("groundMaterial", scene);
-    groundMaterial.diffuseColor = new BABYLON.Color3(0.5, 0.8, 0.5); // RGB for a greenish color
+    const ground = MeshBuilder.CreateGround("ground", { width: 12, height: 12 }, scene);
+    const groundMaterial = new StandardMaterial("groundMaterial", scene);
+    groundMaterial.diffuseColor = new Color3(0.5, 0.5, 0.5);
     ground.material = groundMaterial;
-    groundMaterial.bumpTexture = new BABYLON.Texture("./normal.jpg", scene);
-    //groundMaterial.bumpTexture.level = 0.125;    
+    groundMaterial.bumpTexture = new Texture("./normal.jpg", scene);
 
 
-    var redMaterial = new BABYLON.StandardMaterial("redMaterial", scene);
-    redMaterial.diffuseColor = new BABYLON.Color3(1, 0, 0); // RGB for red
+    const redMaterial = new StandardMaterial("redMaterial", scene);
+    redMaterial.diffuseColor = new Color3(1, 0, 0);
     sphere.material = redMaterial;
-
-    var sphereVelocity = 0;
-    var gravity = 0.009;
-    var reboundLoss = 0.1;
-
-    scene.registerBeforeRender(() => {
-        sphereVelocity += gravity;
-        let newY = sphere.position.y - sphereVelocity;
-        sphere.position.y -= sphereVelocity
-        if (newY < 1) {
-            sphereVelocity = (reboundLoss - 1) * sphereVelocity;
-            newY = 1;
-        }
-        sphere.position.y = newY;
-        if (Math.abs(sphereVelocity) <= gravity && newY < 1 + gravity) {
-            sphere.position.y = startPos++;
-        }
-    });
 
     return scene;
 };
